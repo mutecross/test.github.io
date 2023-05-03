4fc2b505d0cf78a712d1818342d2e692f0b5eee1
Konstantin <mutecross@gmail.com>
2023-05-04 00:13:21 +0300

    Добавлены файлы sticky и fixed, добавлены соответствующие стили, js массив со списком английских слов

---
+++ js/extensions.js
@@ -0,0 +1,14 @@
+function NavigationBlock() {
+    let OUTPUT = '<div class="navigation__menu">';
+    OUTPUT += '<a class="menu__item" href="index.html">Главная</a>';
+    OUTPUT += '<a class="menu__item" href="grid.html">Grid</a>';
+    OUTPUT += '<a class="menu__item" href="flex.html">Flex</a>';
+    OUTPUT += '<a class="menu__item" href="sticky.html">Sticky</a>';
+    OUTPUT += '<a class="menu__item" href="fixed.html">Fixed</a>';
+    OUTPUT += '</div>';
+    $('.navigation').html(OUTPUT);
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
